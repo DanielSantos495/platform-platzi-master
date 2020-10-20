9e9a44b0d0d68dd06c06d8c8665ddf58a7203743
Daniel Santos <dsantos0495@gmail.com>
2020-10-20 09:05:27 -0500

    arreglando rutas2

--- src/app/App.js
+++ src/app/App.js
@@ -9,7 +9,7 @@ import { RegisterForm } from '../components/RegisterForm/index'
 export const App = () => (
   <Layout>
     <GlobalStyles />
-    <Router basepath='/platform-platzi-master/'>
+    <Router basepath='/'>
       <Students path='/' />
       <RegisterForm path='/register' />
     </Router>
